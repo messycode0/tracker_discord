aa94c38a5b38e515e4e0fbe0f9f95720a8f2f618
Jake o'brien <jacobrien64@gmail.com>
2023-03-27 20:16:42 -0700

    able to respond

--- src/tracker.py
+++ src/tracker.py
@@ -1,8 +1,47 @@
 import discord
 from discord.ext import commands
 
-def start(token: str):
-    print("Starting tracker...")
-    print("Token: " + token)
+from rich.console import Console
 
-    
+
+def start(token):
+    intents = discord.Intents().all()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix='/', intents=intents)
+
+    console = Console()
+
+    # print the title saying tracker ascii art
+
+    console.print('''
+
+████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ 
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝
+   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+    ''')
+
+
+
+    @bot.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(bot))
+        print('Bot is ready')
+
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+
+        if message.content.startswith('hello'):
+            await message.channel.send('Hello!')
+
+        await bot.process_commands(message)
+
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send('pong')
+
+    bot.run(token)
