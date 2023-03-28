82a481a4c73ba61c46335077558802895b54ecc9
Jake o'brien <jacobrien64@gmail.com>
2023-03-27 20:39:44 -0700

    can do basic commands

--- src/tracker.py
+++ src/tracker.py
@@ -7,7 +7,7 @@ from rich.console import Console
 def start(token):
     intents = discord.Intents().all()
     intents.message_content = True
-    bot = commands.Bot(command_prefix='/', intents=intents)
+    bot = commands.Bot(command_prefix='--', intents=intents)
 
     console = Console()
 
@@ -23,7 +23,7 @@ def start(token):
    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
     ''')
 
-    console.rule("[bold red]Rich console Initiated[/bold red]", style="bold red", align="left", width=50, characters=("-","")
+    console.rule("[bold red]Rich console Initiated[/bold red]", style="bold red", align="left",)
 
 
 
@@ -31,9 +31,12 @@ def start(token):
     async def on_ready():
         print('We have logged in as {0.user}'.format(bot))
         print('Bot is ready')
+        console.rule("[bold red]Discord Bot Initiated[/bold red]", style="bold red", align="left",)
+        console.print('\nTracker is now ready for its mission\n', style="white", justify="center")
 
     @bot.event
     async def on_message(message):
+        console.print(f'{message.author}\n    [b i ]{message.content}[b i]', style="white", justify="left")
         if message.author == bot.user:
             return
 
@@ -42,8 +45,31 @@ def start(token):
 
         await bot.process_commands(message)
 
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send('Invalid command used.')
+
+    # user deletes a message
+    @bot.event
+    async def on_message_delete(message):
+        console.print(f'{message.author} deleted a message\n    [b i ]{message.content}[b i]', style="red", justify="left")
+        # send message to log channel
+
     @bot.command()
     async def ping(ctx):
         await ctx.send('pong')
+
+    @bot.command()
+    async def clear(ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
+
+    @bot.command()
+    async def exit(ctx, reason="gave none, please try and give one next time"):
+        await ctx.send('Exiting... for reason: ' + reason)
+        await bot.close()
+        return reason
+
+
 
     bot.run(token)
